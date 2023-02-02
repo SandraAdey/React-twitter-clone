ddff1c25dc13b43bbb2e0f0c0452ddf0378ff302
Sandra Adey <sandraadeyinka@gmail.com>
2023-02-02 00:28:12 +0000

    testing git

--- src/Feed.js
+++ src/Feed.js
@@ -5,7 +5,15 @@ import './Feed.css';
 import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import db from './firebase';
 
+
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot => (
+    setPosts(snapshot.docs.map((doc) => doc.data()))
+  ))
+  }, []);
 
   return (
     <div className="feed">
@@ -17,22 +25,19 @@ function Feed() {
       </div>
 
       <TweetBox />
-
-      <Post
-        displayName="sandra adeyinka"
-        username="sandraaa"
-        verified={true}
-        text="yooo its working"
-        avatar="https://images.pexels.com/photos/4587959/pexels-photo-4587959.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        image={"https://media.giphy.com/media/L4lvBzeiy0z6w/giphy.gif"}
+      {posts.map((post) => (
+      <Post 
+        displayName={post.displayName}
+        username={post.username}
+        verified={post.verified}
+        text={post.text}
+        avatar={post.avatar}
+        image={post.image}
       />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+        ))} 
+
     </div>
   );
 }
 
-export default Feed
+export default Feed;
