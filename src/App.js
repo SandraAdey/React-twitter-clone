7ef0ee284692f4be815ea5c004aa1a1476fe5cd8
Sandra Adey <sandraadeyinka@gmail.com>
2023-04-17 21:41:30 +0100

    massive updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import Widgets from "./Widgets";
 import "./App.css";
-
 
 function App() {
   return (
     // BEM
     <div className="app">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* feed */}
       <Feed />
-      
-      {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,45 @@
+import React, { forwardRef } from "react";
+import "./Post.css";
+import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import LoopIcon from "@mui/icons-material/Loop";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PublishIcon from "@mui/icons-material/Publish";
+
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post__avatar">
+          <Avatar src={avatar} />
+        </div>
+        <div className="post__body">
+          <div className="post__header">
+            <div className="post__headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />} @
+                  {username}
+                </span>
+              </h3>
+            </div>
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
+          </div>
+          <img src={image} alt="" />
+          <div className="post__footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <LoopIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default Post;
